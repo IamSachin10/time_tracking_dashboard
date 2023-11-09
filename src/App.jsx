a796347170fa7e54e00000565d1b85f0b6da41e3
Sachin <sachin333777@outlook.com>
2023-11-09 15:24:00 +0530

    admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import jeremyPicture from './images/image-jeremy.png'
+function App(){
+  return (
+    <>
+  <header className='capitalize bg-darkBlue rounded-t-xl rounded-b-xl'>
+    <div className='flex items-center justify-center bg-Blue gap-2 rounded-xl p-6'>
+      <img src={jeremyPicture} alt="admin-profile-picture" className=' block w-16' />
+      <div>
+        <p>report for</p>
+        <h1>jeremy robson</h1>
+      </div>
+    </div>
+    <div className='flex justify-between text-sm p-4 cursor-pointer text-desaturatedBlue'>
+      <p className=' hover:text-white'>daily</p>
+      <p className=' hover:text-white'>weekly</p>
+      <p className=' hover:text-white'>monthly</p>
+    </div>
+  </header>
+    </>
+  )
+}
 
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,39 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary colors
+        'Blue': 'hsl(246,80%,60%)',
+        'LightRedWork': 'hsl(15,100%,70%)',
+        'SoftBlue': 'hsl(195,74%,62%)',
+        'LightRedStudy': 'hsl(348,100%,68%)',
+        'LimeGreen': 'hsl(145,58%,55%)',
+        'Violet': 'hsl(264,64%,52%)',
+        'SoftOrange': 'hsl(43,84%,65%)',
+        // Neutral colors
+        'veryDarkBlue': 'hsl(226,43%,10%)',
+        'darkBlue': 'hsl(235,46%,20%)',
+        'desaturatedBlue': 'hsl(235,45%,61%)',
+        'paleBlue': 'hsl(236,100%,87%)'
+      },
+      fontFamily: {
+        'Rubik': ["'Rubik', serif"]
+      },
+      backgroundImage: {
+        'profile': 'url(./src/images/image-jeremy.png)',
+        'icon-ellipsis': 'url(./src/images/icon-ellipsis.svg)',
+        'icon-exercise': 'url(./src/images/icon-exercise.svg)',
+        'icon-play': 'url(./src/images/icon-play.svg)',
+        'icon-self-care': 'url(./src/images/icon-self-care.svg)',
+        'icon-social': 'url(./src/images/icon-social.svg)',
+        'icon-study': 'url(./src/images/icon-study.svg)',
+        'icon-work': 'url(./src/images/icon-work.svg)',
+      },
+      fontSize: {
+        '18px': '1.125rem'
+      }
+    },
   },
   plugins: [],
 }

da15d75073187f5d23dd7dee3b4824ba375fbff2
Sachin <sachin333777@outlook.com>
2023-11-20 20:46:59 +0530

    change text style

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
         </div>
       </header>
       {/* dashboard */}
-      <section className="my-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:my-0">
+      <section className="my-2 text-lg sm:grid sm:grid-cols-3 sm:gap-4 sm:my-0">
         {jsonData.map((item, index) => (
           <article
             // className=" bg-LightRedWork rounded-b-xl rounded-t-xl my-2"
@@ -83,27 +83,27 @@ function App() {
               {/* <img src="" alt="" /> */}
             </div>
             <div className="bg-darkBlue rounded-t-xl rounded-b-xl px-4 py-8 sm:h-[27vh]">
-              <div className=" flex justify-between">
-                <h2>{item.title}</h2>
-                <p>...</p>
+              <div className=" flex justify-between items-center">
+                <h2 className="sm:font-normal">{item.title}</h2>
+                <p className="sm:text-xl">...</p>
               </div>
               <div>
                 {hours === "daily" && (
-                  <div className=" flex justify-between sm:flex-col sm:mt-4">
-                    <p>{item.timeframes.daily.current}hrs</p>
-                    <p>last day - {item.timeframes.daily.previous}hrs</p>
+                  <div className=" flex justify-between sm:flex-col sm:mt-4 sm:gap-2">
+                    <p className="sm:text-5xl">{item.timeframes.daily.current}hrs</p>
+                    <p className="text-[#A9A9A9]">last day - {item.timeframes.daily.previous}hrs</p>
                   </div>
                 )}
                 {hours === "weekly" && (
-                  <div className=" flex justify-between">
-                    <p>{item.timeframes.weekly.current}hrs</p>
-                    <p>last week - {item.timeframes.weekly.previous}hrs</p>
+                  <div className=" flex justify-between sm:flex-col sm:mt-4 sm:gap-2">
+                    <p className="sm:text-5xl">{item.timeframes.weekly.current}hrs</p>
+                    <p className="text-[#A9A9A9]" >last week - {item.timeframes.weekly.previous}hrs</p>
                   </div>
                 )}
                 {hours === "monthly" && (
-                  <div className="flex justify-between">
-                    <p>{item.timeframes.monthly.current}hrs</p>
-                    <p>last month - {item.timeframes.monthly.previous}hrs</p>
+                  <div className="flex justify-between sm:flex-col sm:mt-4 sm:gap-2 ">
+                    <p className="sm:text-5xl">{item.timeframes.monthly.current}hrs</p>
+                    <p className="text-[#A9A9A9]">last month - {item.timeframes.monthly.previous}hrs</p>
                   </div>
                 )}
               </div>
